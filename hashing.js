7af4104103d952f1a9f7a15867c1a3212a2e8b1e
Bikramjit <bikdev07@gmail.com>
2022-05-29 12:29:05 +0530

    after authentication

---
+++ hashing.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+//hashing function encryption
+require('dotenv').config();
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
+const md5=require("md5");
+const app=express();
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+//--------database------------
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
+
+const userSchema=new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+
+
+const User=new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home")
+});
+//console.log(process.env.API_KEY);
+app.get("/register",function(req,res){
+    res.render("register")
+});
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/register",function(req,res){
+    const newUser=new User({
+        email:req.body.username,
+        password:md5(req.body.password)
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login",function(req,res){
+    const email=req.body.username;
+    const password=md5(req.body.password);
+
+    User.findOne({email:email},function(err,founduser){
+        if(err){
+            console.log(err)
+        }
+        else{
+            if(founduser){
+                if(founduser.password===password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+
+
+
+
+app.listen(process.env.PORT || 5000,function(){
+    console.log("server started!!!!!")
+});
